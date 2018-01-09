7b80846087179b4485503089324b85b1d1283114
yao <yaopaine@outlook.com>
2018-01-09 12:20:40 +0800

    注解练习项目初始化

---
+++ app/src/main/java/com/yaopaine/annotationpractise/annotation/util/ViewInjectUtils.java
@@ -0,0 +1,10 @@
+package com.yaopaine.annotationpractise.annotation.util;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class ViewInjectUtils {
+
+    public static void inject(AppCompatActivity activity) {
+
+    }
+}
