877407ccc0b8a8312a5ab7111f3a0bd0ba430165
yao <yaopaine@outlook.com>
2018-01-11 17:48:47 +0800

    commit

--- app/src/main/java/com/yaopaine/annotationpractise/MainActivity.java
+++ app/src/main/java/com/yaopaine/annotationpractise/MainActivity.java
@@ -1,6 +1,8 @@
 package com.yaopaine.annotationpractise;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -25,9 +27,12 @@ public class MainActivity extends AppCompatActivity {
     @ViewInject(R.id.tv_content3)
     public TextView tvContent3;
 
+    private String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.e(TAG, "onCreate: ");
         ViewInjectUtils.inject(this);
         //setContentView(R.layout.activity_main);
         tvTitle.setText("这是标题");
@@ -38,6 +43,58 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick({R.id.tv_content1, R.id.tv_content2, R.id.tv_content3})
     private void onClick(View view) {
+        if (view.getId() == R.id.tv_content1) {
+            startActivity(new Intent(this, SecondActivity.class));
+        }
         Log.e("TAG", "onClick: " + ((TextView) view).getText());
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        Log.e(TAG, "onPostCreate: ");
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e(TAG, "onRestart: ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.e(TAG, "onPostResume: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG, "onDestroy: ");
     }
 }
---
+++ app/src/main/java/com/yaopaine/annotationpractise/SecondActivity.java
@@ -0,0 +1,66 @@
+package com.yaopaine.annotationpractise;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private String TAG = "SecondActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        Log.e(TAG, "onPostCreate: ");
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e(TAG, "onRestart: ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.e(TAG, "onPostResume: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG, "onDestroy: ");
+    }
+}
